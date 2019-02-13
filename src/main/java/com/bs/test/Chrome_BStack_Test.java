a4b5e73c02bb2ccc6c79c31208d871fd445c9bb6
C_Karadkar <c.karadkar@gmail.com>
2019-02-14 00:20:23 +0530

    update-1

--- src/main/java/com/bs/test/Chrome_BStack_Test.java
+++ src/main/java/com/bs/test/Chrome_BStack_Test.java
@@ -15,29 +15,29 @@ public class Chrome_BStack_Test {
 		String BROWSERSTACK_USERNAME = System.getenv("BROWSERSTACK_USERNAME");
 		String BROWSERSTACK_ACCESS_KEY = System.getenv("BROWSERSTACK_ACCESS_KEY");
 		
-		String HUB_URL = "http://"+BROWSERSTACK_USERNAME+":"+BROWSERSTACK_ACCESS_KEY+"@localhost:4444/wd/hub";	// Selenium HUB URL
+		String HUB_URL = "http://"+BROWSERSTACK_USERNAME+":"+BROWSERSTACK_ACCESS_KEY+"@localhost:4444/wd/hub"; // Selenium HUB URL
 		
-		String AUT_URL = "https://the-internet.herokuapp.com/login";	// URL of APPLICATION UNDER TEST
+		String AUT_URL = "https://the-internet.herokuapp.com/login"; // URL of APPLICATION UNDER TEST
 		
-		URL URLObj = new URL(HUB_URL);			// URL object represents a Uniform Resource Locator, a pointer to a "resource" on the World Wide Web
+		URL URLObj = new URL(HUB_URL); // URL object represents a Uniform Resource Locator, a pointer to a "resource" on the World Wide Web
 
-		DesiredCapabilities caps = new DesiredCapabilities(); 	// CREATE DESIRED CAPABILITIES OBJECT
+		DesiredCapabilities caps = new DesiredCapabilities(); // CREATE DESIRED CAPABILITIES OBJECT
 		
-		caps.setCapability("browserName", "chrome"); 			// PROVIDE BROWSER NAME
+		caps.setCapability("browserName", "chrome"); // PROVIDE BROWSER NAME
 		
-		WebDriver driver = new RemoteWebDriver(URLObj, caps); 	// CREATE WEN DRIVER INSTANCE
+		WebDriver driver = new RemoteWebDriver(URLObj, caps); // CREATE WEN DRIVER INSTANCE
 		
-//		driver.manage().window().maximize();					// MAXIMIZE WINDOW
+//		driver.manage().window().maximize(); // MAXIMIZE WINDOW
 		
-		driver.get(AUT_URL);									// OPEN URL
+		driver.get(AUT_URL); // OPEN URL
 	
 		WebElement element = driver.findElement(By.xpath(".//button[@type='submit']")); // FIND THE ELEMENT
 		
-		element.click();										// PERFORM CLICK OPERATION
+		element.click(); // PERFORM CLICK OPERATION
 		
 		Thread.sleep(2000);
 		
-		driver.quit();											// CLOSE WEB DRIVER INSTANCE
+		driver.quit(); // CLOSE WEB DRIVER INSTANCE
 		
 	} // MAIN END
 
--- src/main/java/com/bs/test/Chrome_Local_Test.java
+++ src/main/java/com/bs/test/Chrome_Local_Test.java
@@ -12,29 +12,29 @@ public class Chrome_Local_Test {
 	
 	public static void main(String[] args) throws Exception {
 		
-		String HUB_URL = "http://localhost:4446/wd/hub";				// Selenium HUB URL
+		String HUB_URL = "http://localhost:4446/wd/hub"; // Selenium HUB URL
 		
-		String AUT_URL = "https://the-internet.herokuapp.com/login";	// URL of APPLICATION UNDER TEST
+		String AUT_URL = "https://the-internet.herokuapp.com/login"; // URL of APPLICATION UNDER TEST
 		
-		URL URLObj = new URL(HUB_URL);			// URL object represents a Uniform Resource Locator, a pointer to a "resource" on the World Wide Web
+		URL URLObj = new URL(HUB_URL); // URL object represents a Uniform Resource Locator, a pointer to a "resource" on the World Wide Web
 
-		DesiredCapabilities caps = new DesiredCapabilities(); 	// CREATE DESIRED CAPABILITIES OBJECT
+		DesiredCapabilities caps = new DesiredCapabilities(); // CREATE DESIRED CAPABILITIES OBJECT
 		
-		caps.setCapability("browserName", "chrome"); 			// PROVIDE BROWSER NAME
+		caps.setCapability("browserName", "chrome"); // PROVIDE BROWSER NAME
 		
-		WebDriver driver = new RemoteWebDriver(URLObj, caps); 	// CREATE WEN DRIVER INSTANCE
+		WebDriver driver = new RemoteWebDriver(URLObj, caps); // CREATE WEN DRIVER INSTANCE
 		
-//		driver.manage().window().maximize();					// MAXIMIZE WINDOW
+//		driver.manage().window().maximize(); // MAXIMIZE WINDOW
 		
-		driver.get(AUT_URL);									// OPEN URL
+		driver.get(AUT_URL); // OPEN URL
 	
 		WebElement element = driver.findElement(By.xpath(".//button[@type='submit']")); // FIND THE ELEMENT
 		
-		element.click();										// PERFORM CLICK OPERATION
+		element.click(); // PERFORM CLICK OPERATION
 		
 		Thread.sleep(2000);
 		
-		driver.quit();											// CLOSE WEB DRIVER INSTANCE
+		driver.quit(); // CLOSE WEB DRIVER INSTANCE
 		
 	} // MAIN END
 
